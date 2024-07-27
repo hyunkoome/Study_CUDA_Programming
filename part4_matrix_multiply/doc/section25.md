9776c902f620faff5c2361aabcb78a355dc02cb8
hyunkoome <hyunkookim.me@gmail.com>
2024-07-27 11:29:50 +0900

    added sec25

--- part4_matrix_multiply/doc/section25.md
+++ part4_matrix_multiply/doc/section25.md
@@ -1,23 +1,46 @@
 # Memory Structure
 
-***matrix copy – theoretical limit***
-- CPU version
-- memcpy version
-- CUDA naive version – global memory
-  - naive : (전문적) 지식이 없는
-- CUDA shared memory version
-- CUDA memcpy2D
+***GEMM***
+- general matrix-to-matrix multiplication
+  - Z = alpha A B + beta C
+    - A, B, C, Z: matrices
+    - alpha, beta: scalar values
+  - `핵심`은 `행렬 곱하기`를 어떻게하면 빠르게 하느냐 이다.
+    - 행렬 곱하기 강의 (`Section 24`를 복습!!)
+- BLAS level 3 operation
+  - BLAS = Basic Linear Algebra 기능들(Subprograms)
+  - BLAS 함수 중에서 가장 많이 쓰이고.
+  - BLAS optimization 최적화에서 가장 중요하게 고려
 
-***Matrix Copy***
-- simply copy a matrix to another
-  - C[i,j] = A[i,j]
-  - `pitched matrices` for the best performance
-  - for simplicity, we assume `square matrices`
-- 다른 matrix 연산의 입장에서는?
-  - theoretical limit 이론적 한계for matrix operations
-  - the best score 최고 기록 for any matrix operations 행렬 대입 연산이 최고 기록이다.
-  - 따라서 다른 연산, 즉 더하기 연산 등은 이 행렬 대입연산의 속도에 근접할 수록 최적화가 잘 된것이고
-  - 이 기록을 추월했다면, 만세를 부를게 아니라, 뭔가 잘 못된 것임
-    - 즉, 연산의 물체를 발견할 수 있는 어떤 기준이 될수도 있음
+***implementations (4k x 4k)***
+- cpu: `gemm_cpu.cu`
+  - 3중 for-loop
+  - 실행 시간: 410,418,710 usec = 410초 = 6분 50초
+- cpu outer-k `gemm_cpu_outerK.cu`
+  - for-loop 순서 변경
+  - 당연히, 순서 바꾸면 `10배 정도는 빨라져야 함!!`
+  - 실행 시간: 91,769,364 = 91초 = 1분 31초 
+- CUDA `gemm_cuda_globalmem.cu`
+  - cuda global 메모리 사용 버전
+  - grid 설계: 2D layout
+    - matrix C 의 각 원소 (y,x) 마다 thread (block) 생성
+    - 각 thread 는 for loop 로 row x column 계산
+    - 3중 for loop 에서 바깥쪽 2중 for loof 는 `2D grid (block thread) 구조`로 대체
+      - 안쪽의 하나의 for 루프만 커널 내에서 실행 하도록..
+    - 이 모든 메모리 구조가 다 `pitched` 되어있다는 것은 신경써야 함!!
+  - 실행 시간: 
+    - (4k x 4k): 33,401 usec = 0.033 초
+    - (8k x 8k): 330,170 usec = 0.330 초
+- CUDA aligned tile `gemm_cuda_alignedTile.cu`
+  - tile 크기를 32x32 로 가정
+  - matrix 크기 를 32의 배수로 가정, (4k x 4k 사용)
+  - 실행 시간
+    - (4k x 4k): 36,619 usec = 0.036 초
+    - (8k x 8k): 267,321 usec = 0.267 초
+- CUDA general tile `gemm_cuda_tile.cu`
+  - matrix 크기를 임의로 변경 가능 (4000 x 4000 도 가능)
+  - 실행 시간
+    - (4000 x 4000): 35,769 usec = 0.035 초
+    - (8000 x 8000): 260,448 usec = 0.260 초
 
 [Return Par4 Matrix Multiply](../README.md)  
---
+++ part4_matrix_multiply/section_25_GEMM_general_matrix_to_matrix_multiplication/gemm_cuda_tile.cu
@@ -0,0 +1,172 @@
+#include "./common.cpp"
+
+// input parameters
+const float alpha = 0.5f;
+const float beta = -100.0f;
+const unsigned TILE_WIDTH = 32;
+unsigned matsize = 4096; // num rows and also num cols
+
+// CUDA kernel function
+__global__ void kernelGEMM( float* Z, const float* A, const float* B, const float* C,
+                            unsigned matsize, size_t pitch_in_elem, const float alpha, const float beta ) {
+	__shared__ float s_A[TILE_WIDTH][TILE_WIDTH];
+	__shared__ float s_B[TILE_WIDTH][TILE_WIDTH];
+	register unsigned ntiles = (matsize + TILE_WIDTH - 1) / TILE_WIDTH;
+	register unsigned remaining = matsize; // remained elements to be multiplied
+	register unsigned gy = blockIdx.y * blockDim.y + threadIdx.y; // y-coord
+	register unsigned gx = blockIdx.x * blockDim.x + threadIdx.x; // x-coord
+	register float sum = 0.0f;
+	for (register unsigned tile = 0; tile < ntiles; ++tile) {
+		register unsigned nelem = min( remaining, TILE_WIDTH );
+		remaining -= TILE_WIDTH;
+		if (gy < matsize && threadIdx.x < nelem) {
+			register unsigned idxA = gy * pitch_in_elem + (tile * TILE_WIDTH + threadIdx.x);
+			s_A[threadIdx.y][threadIdx.x] = A[idxA];
+#if defined(_DEBUG)
+		} else {
+			s_A[threadIdx.y][threadIdx.x] = 0.0f;
+#endif
+		}
+		if (gx < matsize && threadIdx.y < nelem) {
+			register unsigned idxB = (tile * TILE_WIDTH + threadIdx.y) * pitch_in_elem + gx;
+			s_B[threadIdx.y][threadIdx.x] = B[idxB];
+#if defined(_DEBUG)
+		} else {
+			s_B[threadIdx.y][threadIdx.x] = 0.0f;
+#endif
+		}
+		__syncthreads();
+		for (register unsigned k = 0; k < nelem; ++k) {
+			sum += s_A[threadIdx.y][k] * s_B[k][threadIdx.x];
+		}
+		__syncthreads();
+	}
+	if (gy < matsize && gx < matsize) {
+		register unsigned idxC = gy * pitch_in_elem + gx;
+		Z[idxC] = alpha * sum + beta * C[idxC];
+	}
+}
+
+int main(const int argc, const char* argv[]) {
+	// argv processing
+	switch (argc) {
+	case 1:
+		break;
+	case 2:
+		matsize = procArg( argv[0], argv[1], 4 );
+		break;
+	default:
+		printf("usage: %s [matsize]\n", argv[0]);
+		exit(EXIT_FAILURE); // EINVAL: invalid argument
+		break;
+	}
+	// host-side data
+	float* matA = nullptr;
+	float* matB = nullptr;
+	float* matC = nullptr;
+	float* matZ = nullptr;
+	float* matW = nullptr;
+	try {
+		matA = new float[matsize * matsize];
+		matB = new float[matsize * matsize];
+		matC = new float[matsize * matsize];
+		matZ = new float[matsize * matsize];
+		matW = new float[matsize * matsize];
+	} catch (const exception& e) {
+		printf("C++ EXCEPTION: %s\n", e.what());
+		exit(EXIT_FAILURE);
+	}
+	// set random data
+	srand( 0 );
+	setNormalizedRandomData( matA, matsize * matsize );
+	setNormalizedRandomData( matB, matsize * matsize );
+	setNormalizedRandomData( matC, matsize * matsize );
+	// device-side data
+	float* dev_matA = nullptr;
+	float* dev_matB = nullptr;
+	float* dev_matC = nullptr;
+	float* dev_matZ = nullptr;
+	// allocate device memory
+	ELAPSED_TIME_BEGIN(1);
+	size_t host_pitch = matsize * sizeof(float); // host side: packed compactly
+	size_t dev_pitch = 0;
+	cudaMallocPitch( (void**)&dev_matA, &dev_pitch, matsize * sizeof(float), matsize );
+	cudaMallocPitch( (void**)&dev_matB, &dev_pitch, matsize * sizeof(float), matsize );
+	cudaMallocPitch( (void**)&dev_matC, &dev_pitch, matsize * sizeof(float), matsize );
+	cudaMallocPitch( (void**)&dev_matZ, &dev_pitch, matsize * sizeof(float), matsize );
+	printf("dev_pitch = %zd byte, host_pitch = %zd byte\n", dev_pitch, host_pitch);
+	CUDA_CHECK_ERROR();
+	// copy to device from host
+	cudaMemcpy2D( dev_matA, dev_pitch, matA, host_pitch, matsize * sizeof(float), matsize, cudaMemcpyHostToDevice);
+	cudaMemcpy2D( dev_matB, dev_pitch, matB, host_pitch, matsize * sizeof(float), matsize, cudaMemcpyHostToDevice);
+	cudaMemcpy2D( dev_matC, dev_pitch, matC, host_pitch, matsize * sizeof(float), matsize, cudaMemcpyHostToDevice);
+	CUDA_CHECK_ERROR();
+	// CUDA kernel launch
+	dim3 dimBlock(32, 32, 1);
+	dim3 dimGrid(div_up(matsize, dimBlock.x), div_up(matsize, dimBlock.y), 1);
+	assert(dev_pitch % sizeof(float) == 0);
+	register unsigned pitch_in_elem = dev_pitch / sizeof(float);
+	CUDA_PRINT_CONFIG_2D( matsize, matsize );
+	ELAPSED_TIME_BEGIN(0);
+	kernelGEMM <<< dimGrid, dimBlock>>>( dev_matZ, dev_matA, dev_matB, dev_matC, matsize, pitch_in_elem, alpha, beta );
+	cudaDeviceSynchronize();
+	ELAPSED_TIME_END(0);
+	CUDA_CHECK_ERROR();
+	// copy to host from device
+	cudaMemcpy2D( matZ, host_pitch, dev_matZ, dev_pitch, matsize * sizeof(float), matsize, cudaMemcpyDeviceToHost);
+	ELAPSED_TIME_END(1);
+	CUDA_CHECK_ERROR();
+	// free device memory
+	cudaFree( dev_matA );
+	cudaFree( dev_matB );
+	cudaFree( dev_matC );
+	cudaFree( dev_matZ );
+	CUDA_CHECK_ERROR();
+	// check the result
+	float sumA = getSum( matA, matsize * matsize );
+	float sumB = getSum( matB, matsize * matsize );
+	float sumC = getSum( matC, matsize * matsize );
+	float sumZ = getSum( matZ, matsize * matsize );
+	printf("matrix size = matsize * matsize = %d * %d\n", matsize, matsize);
+	printf("sumA = %f\n", sumA);
+	printf("sumB = %f\n", sumB);
+	printf("sumC = %f\n", sumC);
+	printf("sumZ = %f\n", sumZ);
+	printMat( "matZ", matZ, matsize, matsize );
+	printMat( "matA", matA, matsize, matsize );
+	printMat( "matB", matB, matsize, matsize );
+	printMat( "matC", matC, matsize, matsize );
+#if 0
+	memset( matW, 0, matsize * matsize * sizeof(float) );
+	for (register unsigned k = 0; k < matsize; ++k) {
+		for (register unsigned r = 0; r < matsize; ++r) {
+			for (register unsigned c = 0; c < matsize; ++c) {
+				unsigned indZ = r * matsize + c; // convert to 1D index
+				unsigned indA = r * matsize + k; // convert to 1D index
+				unsigned indB = k * matsize + c; // convert to 1D index
+				matW[indZ] += matA[indA] * matB[indB];
+			}
+		}
+	}
+	for (register unsigned r = 0; r < matsize; ++r) {
+		for (register unsigned c = 0; c < matsize; ++c) {
+			unsigned indW = r * matsize + c; // convert to 1D index
+			matW[indW] = alpha * matW[indW] + beta * matC[indW];
+		}
+	}
+	float sumW = getSum( matW, matsize * matsize );
+	float rmsError = getRMS( matW, matZ, matsize * matsize );
+	printf("sumW = %f\n", sumW);
+	printf("rmsErr(matZ, matW) = %f\n", rmsError);
+#endif
+	// cleaning
+	delete[] matA;
+	delete[] matB;
+	delete[] matC;
+	delete[] matZ;
+	delete[] matW;
+	// done
+	return 0;
+}
+
+/* (c) 2021-2022. biztripcru@gmail.com. All rights reserved. */
